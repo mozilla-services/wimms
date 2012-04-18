cea155dab4d7eb7ca90f1a89d443d74ec1c6b27d
Tarek Ziade <tarek@ziade.org>
2012-04-18 11:39:08 +0200

    cleanup

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
-
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
 import os
 from setuptools import setup, find_packages
 
@@ -14,8 +16,8 @@ requires = ['unittest2', 'mozsvc']
 
 
 setup(name='wimms',
-      version='0.1.0',
-      description='tokenlib',
+      version='0.1',
+      description='Where Is My Mozilla Service ?',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
         "Programming Language :: Python",
@@ -23,10 +25,10 @@ setup(name='wimms',
       author='Mozilla Services',
       author_email='services-dev@mozilla.org',
       url='https://github.com/mozilla-services/wimms',
-      keywords='authentication token',
+      keywords='user database',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
       install_requires=requires,
       tests_require=requires,
-      test_suite="tokenlib")
+      test_suite="wimms")
