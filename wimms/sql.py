3ad6353546a72989e7e51f82956d703adc82555b
Tarek Ziade <tarek@ziade.org>
2012-07-18 22:33:42 +0200

    changed allocate_node() behavior: if the node is already allocated, just return it

--- wimms/sql.py
+++ wimms/sql.py
@@ -123,15 +123,20 @@ class SQLMetadata(object):
             res.close()
 
     def allocate_node(self, email, service):
-        if self.get_node(email, service) != (None, None):
-            raise BackendError("Node already assigned")
+        uid, node = self.get_node(email, service)
+        if (uid, node) != (None, None):
+            return uid, node
 
         # getting a node
         node = self.get_best_node(service)
 
         # saving the node
-        res = self._safe_execute(_INSERT, email=email, service=service,
-                                 node=node)
+        try:
+            res = self._safe_execute(_INSERT, email=email, service=service,
+                                     node=node)
+        except IntegrityError:
+            return self.get_node(email, service)
+
         lastrowid = res.lastrowid
         res.close()
 
