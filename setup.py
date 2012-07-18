6c4061636ffc78bb5bd8f4f498323dd9852d9f0d
Tarek Ziade <tarek@ziade.org>
2012-07-18 22:38:21 +0200

    prepped 0.2.1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ requires = ['unittest2', 'mozsvc']
 
 
 setup(name='wimms',
-      version='0.2',
+      version='0.2.1',
       description='Where Is My Mozilla Service ?',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
