4f26efe3593998b6a6a9783041cee5ce6de5edf4
Ben Bangert <ben@groovie.org>
2014-03-28 13:53:48 -0700

    More prep work.

--- wimms/dynamodb.py
+++ wimms/dynamodb.py
@@ -4,3 +4,33 @@
 """DynamoDB backend
 
 """
+import boto.dynamodb2
+from boto.dynamodb2.fields import HashKey
+from boto.dynamodb2.table import Table
+
+from mozsvc.exceptions import BackendError
+
+
+def _dynamo_create_tables():
+
+
+def _dynamo_tables():
+    return (
+        Table()
+    )
+
+
+class DynamoDB(object):
+    """DynamoDB backend for node assignment"""
+    def __init__(self, create_tables=False):
+        if create_tables:
+            _dynamo_create_tables()
+        self._users, self._nodes, self._services = _dynamo_user_table()
+
+    def get_user(self, service, email):
+
+    def create_user(self, service, email, generation=0, client_state=''):
+
+    def update_user(self, service, user, generation=None, client_state=None):
+
+    def get_patterns(self):
